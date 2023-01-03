ba62ed20ff0a39155ff464842a2457301e11e21f
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-03 05:35:42 +0100

    CORS test 13

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var express = require('express')
 var path = require('path');
 var http = require('http');
 var cors = require('cors');
@@ -17,27 +18,8 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*, http://localhost:4200/, https://tomaskre.github.io/PWAFE/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
 app.use(cors({
-    origin: false, //Boolean - set origin to true to reflect the request origin,
+    origin: true, //Boolean - set origin to true to reflect the request origin,
 	//as defined by req.header('Origin'), or set it to false to disable CORS.
 
     credentials: false, // This MUST be "true" if your endpoint is
