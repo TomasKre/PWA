d78ee83f6229f822d47df51dcc302ec85324d6dc
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-03 04:10:12 +0100

    CORS test 3

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var options = {
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
+app.options('*', cors()); // pro pre-flight dotazy?
 app.use(cors({
     origin: true, // "true" will copy the domain of the request back
                   // to the reply. If you need more control than this
@@ -26,8 +27,9 @@ app.use(cors({
                        // or Authorization header. Otherwise the
                        // browser will block the response.
 
-    methods: 'POST,GET,PUT,OPTIONS,DELETE,PATCH' // Make sure you're not blocking
+    methods: 'POST,GET,PUT,OPTIONS,DELETE,PATCH', // Make sure you're not blocking
                                            // pre-flight OPTIONS requests
+	optionsSuccessStatus: 200
 }));
 
 // Initialize the Swagger middleware
