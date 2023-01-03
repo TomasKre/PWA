b0e2909edadf66675d60b8181b113266f3dd8d41
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-03 05:05:35 +0100

    CORS test 7

--- index.js
+++ index.js
@@ -17,17 +17,23 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 //app.options('*', cors()); // pro pre-flight dotazy?
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "");
+  next();
+});
 app.use(cors({
-    origin: false, //Boolean - set origin to true to reflect the request origin,
+    origin: true, //Boolean - set origin to true to reflect the request origin,
 	//as defined by req.header('Origin'), or set it to false to disable CORS.
 
-    credentials: true, // This MUST be "true" if your endpoint is
+    credentials: false, // This MUST be "true" if your endpoint is
                        // authenticated via either a session cookie
                        // or Authorization header. Otherwise the
                        // browser will block the response.
 
     methods: ['POST', 'GET', 'PUT', 'OPTIONS', 'DELETE', 'PATCH'], // Make sure you're not blocking
                                            // pre-flight OPTIONS requests
+	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', '*'],
 	optionsSuccessStatus: 200
 }));
 
