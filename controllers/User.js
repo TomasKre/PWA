bdb8f75053928b409a8d345d6d2e4d46ba3e5c67
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-15 19:33:13 +0100

    Changed response body for login

--- controllers/User.js
+++ controllers/User.js
@@ -29,8 +29,6 @@ exports.getUserByName = function getUserByName (req, res, next) {
 exports.loginUser = function loginUser (req, res, next) {
   User.loginUser(req.body.username, req.body.password)
     .then(function (response) {
-      console.log(response);
-      //var responseObject = {code: response, message: "User logged in"}
 
       const token = jwt.sign({ username: req.body.username }, "PWA_very_secure11", { // secret
         expiresIn: 86400, // 24 hours
@@ -38,7 +36,9 @@ exports.loginUser = function loginUser (req, res, next) {
 
       req.session.token = token;
 
-      utils.writeJson(res, token);
+      var responseObject = {token: token}
+
+      utils.writeJson(res, responseObject);
     })
     .catch(function (response) {
       console.log(response);
