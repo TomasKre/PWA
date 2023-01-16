b8ee2d77cb349c6281678b8843dd92137888085f
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-16 03:40:22 +0100

    Fix typo

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/message', [authJWT.verifyToken], messageController.postMessage);
 
 const server = http.createServer(app);
 
-const io = require("socket.io").listen(serverIO);
+const io = require("socket.io").listen(server);
 io.on('connection', (socket) => {
   console.log('User connected');
 
